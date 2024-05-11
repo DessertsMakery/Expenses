1aca23af1bfceab13a6dfa08a0d037cc85311c86
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-05-11 13:11:39 +0300

    feat: add persistence

---
+++ src/DessertsMakery.Expenses.Persistence/Entities/MongoEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DessertsMakery.Expenses.Persistence.Entities;
+
+public class MongoEntity
+{
+    protected MongoEntity() { }
+
+    [BsonId]
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public Audit Audit { get; set; } = new();
+}
