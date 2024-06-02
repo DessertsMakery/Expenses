eb71a82eecb3ec3f127c754e6fe120f39ac7ce89
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-06-02 13:31:52 +0300

    fix: warnings & test errors

--- src/Common/DessertsMakery.Common.Utility/Extensions/ConfigurationExtensions.cs
+++ src/Common/DessertsMakery.Common.Utility/Extensions/ConfigurationExtensions.cs
@@ -6,8 +6,8 @@ namespace DessertsMakery.Common.Utility.Extensions;
 public static class ConfigurationExtensions
 {
     public static IServiceCollection AddConfiguration<T>(
-        this IServiceCollection? services,
-        IConfiguration? configuration,
+        this IServiceCollection services,
+        IConfiguration configuration,
         string? name = null
     )
         where T : class => services.Configure<T>(configuration.GetSection(name ?? typeof(T).Name));
